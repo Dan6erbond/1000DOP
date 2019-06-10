fbe1a90a68257fcc17be1e4c1cef4cd2d7cb5c85
EyebrowHairs <49742575+EyebrowHairs@users.noreply.github.com>
2019-06-09 23:03:06 -0600

    updated flair templates :D

--- comments.py
+++ comments.py
@@ -99,18 +99,105 @@ def read_comments():
 def get_template_id(day):
     template_id = "109a0770-23e3-11e9-82ff-0ed95a9c69a0"
 
+# new templates, changes every 20 days up to 1000 days
+    if day > 20:
+        template_id = "f24e4c96-626b-11e9-8e57-0ee9be4fc90e"
+    if day > 40:
+        template_id = "166e7920-626c-11e9-a103-0eec3f62b77c"
+    if day > 60:
+        template_id = "305d5b44-626c-11e9-8adc-0e2134b0c082"
+    if day > 80:
+        template_id = "3f851ada-626c-11e9-963b-0e48b74f8626"
     if day > 100:
-        template_id = "bb98a602-1daf-11e9-8a1f-0e6585833b84"
-    if day > 250:
-        template_id = "d99048b8-1daf-11e9-ab91-0e314af13884"
+        template_id = "510a7dfe-626c-11e9-b55f-0e27d1104956"
+    if day > 120:
+        template_id = "a3a2a8b6-626c-11e9-8168-0e26dcafe6e8"
+    if day > 140:
+        template_id = "b566af7a-626c-11e9-bd48-0ea5fb5d6588"
+    if day > 160:
+        template_id = "c8efbad2-626c-11e9-b5f9-0e24ce5715c4"
+    if day > 180:
+        template_id = "daa692b4-626c-11e9-96a2-0e24ce5715c4"
+    if day > 200:
+        template_id = "eff35e7c-626c-11e9-bf2b-0ebede5656ec"
+    if day > 220:
+        template_id = "a760114a-626d-11e9-aa4a-0e7a9c6b88d2"
+    if day > 240:
+        template_id = "b906293e-626d-11e9-9845-0e455eee425a"
+    if day > 260:
+        template_id = "c6f053ee-626d-11e9-9eeb-0e059e0c6668"
+    if day > 280:
+        template_id = "d339cef0-626d-11e9-9124-0ecea79423fa"
+    if day > 300:
+        template_id = "e2356194-626d-11e9-8c64-0e7cd917977e"
+    if day > 320:
+        template_id = "13809728-626e-11e9-8851-0e6b12138c22"
+    if day > 340:
+        template_id = "23bdd6f0-626e-11e9-be25-0ec83fb3c3c8"
+    if day > 360:
+        template_id = "30ac2fba-626e-11e9-b5f4-0e2e9a7f6762"
+    if day > 380:
+        template_id = "3c67aaa0-626e-11e9-ae63-0e918d6792d8"
+    if day > 400:
+        template_id = "48bbb1c0-626e-11e9-90ba-0efc74d5c2fe"
+    if day > 420:
+        template_id = "80135542-626e-11e9-bead-0e99dfb09b6a"
+    if day > 440:
+        template_id = "8df0c8f2-626e-11e9-a67f-0eab45bc0704"
+    if day > 460:
+        template_id = "99c86dba-626e-11e9-bbd3-0e1baf2d8a0a"
+    if day > 480:
+        template_id = "a63ab8b4-626e-11e9-aff0-0e71e991e91a"
     if day > 500:
-        template_id = "f9c34270-1daf-11e9-bb22-0ee8915d3c24"
-    if day > 750:
-        template_id = "08c204b4-1db0-11e9-aae0-0eb3706cb7c0"
-    if day == 1000:
-        template_id = "29ff44fc-1db0-11e9-8f15-0ee0ed89fff4"
+        template_id = "b3e6430c-626e-11e9-bb33-0e096055fb6c"
+    if day > 520:
+        template_id = "c2e05626-626f-11e9-9b32-0e1bdebd9d3a"
+    if day > 540:
+        template_id = "d060ba48-626f-11e9-9fb3-0ee6aea24986"
+    if day > 560:
+        template_id = "37bf708a-6270-11e9-9aa5-0ed56b577244"
+    if day > 580:
+        template_id = "474dfcc4-6270-11e9-8f7b-0e99dfb09b6a"
+    if day > 600:
+        template_id = "54915020-6270-11e9-ad7c-0e4dc3f592fa"
+    if day > 620:
+        template_id = "7b830a34-6270-11e9-9e97-0e7f925c5d12"
+    if day > 640:
+        template_id = "89571b1e-6270-11e9-887f-0e7f80f038d8"
+    if day > 660:
+        template_id = "9505eb5c-6270-11e9-965a-0e607d9e7fcc"
+    if day > 680:
+        template_id = "a1fb479e-6270-11e9-a673-0e3f90a96136"
+    if day > 700:
+        template_id = "b1d64e20-6270-11e9-99f8-0e088ecbae30"
+    if day > 720:
+        template_id = "cc5d09f0-6270-11e9-bb33-0e096055fb6c"
+    if day > 740:
+        template_id = "d94d4080-6270-11e9-86e7-0eb536b4c774"
+    if day > 760:
+        template_id = "e551c90a-6270-11e9-83b1-0ef6e330c46e"
+    if day > 800:
+        template_id = "01c378ea-6271-11e9-a763-0e2cb77c8894"
+    if day > 820:
+        template_id = "3a9bb2fe-6271-11e9-a5ac-0eff4ce9f36c"
+    if day > 840:
+        template_id = "4cb82d28-6271-11e9-9601-0e72e4c65844"
+    if day > 860:
+        template_id = "59263424-6271-11e9-a30c-0e21ef4ee020"
+    if day > 880:
+        template_id = "6625e75a-6271-11e9-bea3-0e3bff7163b2"
+    if day > 900:
+        template_id = "7526631a-6271-11e9-9b39-0ee9be4fc90e"
+    if day > 920:
+        template_id = "cd223166-6271-11e9-a44c-0e3bff7163b2"
+    if day > 940:
+        template_id = "e184a30a-6271-11e9-b1b8-0ef861fe4132"
+    if day > 960:
+        template_id = "f590c57c-6271-11e9-970d-0e6abb8b6296"
+    if day > 980:
+        template_id = "051d164e-6272-11e9-8757-0ea5fb5d6588"
     if day > 1000:
-        template_id = "b9629f32-1e68-11e9-942f-0e21fde54b6e"
+        template_id = "194c574c-6272-11e9-8fd3-0ebecbde42d4"
 
     return template_id
 
